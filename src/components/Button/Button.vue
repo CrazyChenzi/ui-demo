f3c66e7e39dff9080844dc8a2e36cc370e9d21cd
yjc <756319278@qq.com>
2019-01-14 20:51:50 +0800

    add

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button :class="btnClass" @click.prevent="$emit('click', $event)">
+    <slot></slot>
+  </button>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Button extends Vue {
+  @Prop({ type: String, default: 'default' }) private type!: string;
+  @Prop({ type: Boolean, default: false }) private loading!: boolean;
+  public data() {
+    return {};
+  }
+  private get btnClass() {
+    return [
+      'btn',
+      `${'btn-' + this.type}`,
+    ];
+  }
+  private mounted(): void {
+    console.log(this.loading);
+  }
+}
+</script>
+<style src="./Button.sass" lang="sass" scoped>
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <Button>Default</Button>
+    <Button type="primary">Primary</Button>
+    <Button type="primary" :loading="loading" @click="startLoading">
+      <span v-if="!loading">Click me</span>
+      <span v-else>Loading...</span>
+    </Button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Button from '@/components/Button/Button.vue';
+
+@Component({
+  components: {
+    Button,
+  },
+})
+export default class Home extends Vue {
+  private loading: boolean = false;
+  public data() {
+    return {
+      msg: 'welcome ts',
+    };
+  }
+  private startLoading(e: any) {
+    this.loading = true;
+    console.log(e, '这是loading点击事件');
+  }
+}
+</script>
+<style lang="sass" scoped>
+.home 
+  padding: 0 20px
+Button
+  margin-right: 5px
+</style>
+
